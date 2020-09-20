f165cc0d705b4b12b4fc1db08198225a628c7ec8
Paulo Roberto <prbasa@gmail.com>
2020-09-20 10:07:13 -0300

    POST records

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.devsuperior.dspesquisa.entities.Record;
 
 @Repository
-public interface RecordoRepository extends JpaRepository<Record, Long> {
+public interface RecordRepository extends JpaRepository<Record, Long> {
 
 }
