3d25236b574b65825e192691f2bc9c709e4c2777
Paulo Roberto <prbasa@gmail.com>
2020-09-20 23:05:40 -0300

    Adicioando a página de gráficos

---
+++ front-web/src/pages/Charts/types.ts
@@ -0,0 +1,14 @@
+import { Platform } from "../Records/types";
+
+import { Platform } from "../Records/types";
+
+export type Game ={
+    id: number;
+    title: string;
+    platform: Platform;
+}
+
+export type ChartItem = {
+    x: string;
+    y: number;
+}
