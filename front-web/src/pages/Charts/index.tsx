6ed3016b950776c7de476ff625591f4b68eea164
Paulo Roberto <prbasa@gmail.com>
2020-09-20 23:28:01 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -22,7 +22,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-prbasa.herokuapp.com';
 
 
 const Charts = () => {
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -7,7 +7,7 @@ import Pagination from './Pagination'
 import Filters from '../../components/Filters'
 import { Link } from 'react-router-dom';
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-prbasa.herokuapp.com';
 
 const Records = () => {
 
